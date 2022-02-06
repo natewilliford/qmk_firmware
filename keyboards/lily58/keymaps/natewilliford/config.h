f19c29e168475b20bec7fc4d3176d2cc593ebaef
natewilliford <nathan.williford@gmail.com>
2022-02-06 12:51:30 -0800

    Home row mods and Lshift/ent combo.

--- keyboards/lily58/keymaps/natewilliford/config.h
+++ keyboards/lily58/keymaps/natewilliford/config.h
@@ -26,8 +26,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
