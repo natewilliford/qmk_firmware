e8e11e4864ad134c11cb1e773c59010706439156
natewilliford <nathan.williford@gmail.com>
2022-08-02 10:26:52 -0700

    planck layout tweaks

--- keyboards/planck/keymaps/natewilliford/keymap.c
+++ keyboards/planck/keymaps/natewilliford/keymap.c
@@ -17,7 +17,6 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
-
 enum planck_layers {
   _QWERTY,
   _COLEMAK,
@@ -50,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT ,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
     KC_LCTL, KC_APP,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
@@ -60,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,   KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
     KC_ESC,  KC_A,LALT_T(KC_R),LSFT_T(KC_S),LCTL_T(KC_T),KC_G,KC_M,LCTL_T(KC_N),LSFT_T(KC_E),LALT_T(KC_I),KC_O,KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,   KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,   KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
     KC_LEFT, KC_RIGHT,KC_LCTL, KC_LALT, LOWER,   KC_SPC, KC_BSPC, RAISE,   KC_ENT,  KC_RGUI, KC_DOWN, KC_UP
 ),
 
@@ -93,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Raise */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, LCTL(KC_LEFT), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LCTL(KC_RGHT),
+    KC_CAPS, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, LCTL(KC_LEFT), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LCTL(KC_RGHT),
     _______, _______, _______, _______, _______, _______, KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
@@ -139,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_PSLS, KC_P7, KC_P8,   KC_P9,   KC_PMNS, KC_BSPC,
     _______, _______, _______, KC_LPRN, KC_RPRN, _______, KC_PAST, KC_P4, KC_P5,   KC_P6,   KC_PPLS, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2,   KC_P3,   KC_PEQL, _______,
-    _______, _______, _______, _______, TO(0),   KC_SPC,  KC_SPC,  TO(0), KC_P0,   KC_PDOT, _______, _______
+    _______, _______, _______, _______, TO(0),   KC_SPC,  KC_BSPC, TO(0), KC_P0,   KC_PDOT, _______, _______
 ),
 
 [_NAVIGATION] = LAYOUT_planck_grid(
