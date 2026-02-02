1c0e7bedbb5bf1145d956e1b910be4973a1c508d
natewilliford <nathan.williford@gmail.com>
2026-02-01 17:27:12 -0800

    Whatever My last dactyl keymap was.

--- keyboards/handwired/dactyl_manuform/natewilliford/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/natewilliford/keymaps/default/keymap.c
@@ -45,15 +45,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,       KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,  KC_P,    KC_DEL,
     KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,       KC_G,           KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,       KC_B,           KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,
-                      KC_PGUP, KC_PGDN, MO(_LOWER), KC_SPC,         KC_BSPC,  MO(_RAISE), KC_HOME, KC_END,
-                      KC_ENT, KC_LCTL, KC_LALT,    KC_LSFT,        KC_RALT, KC_BSPC, KC_LGUI, KC_RSFT
+                      KC_PGUP, KC_PGDN, MO(_LOWER), KC_SPC,         KC_BSPC, MO(_RAISE), KC_HOME, KC_END,
+                      KC_ENT, KC_LCTL, KC_LALT,    KC_LSFT,         KC_RALT, KC_RCTL, KC_MENU, KC_RGUI
   ),
 
   [_COLMAK] = LAYOUT_split_3x6_3(
-    KC_CAPS, KC_1, KC_2,         KC_3,         KC_4,         KC_5,           KC_6,    KC_7,        KC_8,        KC_9,         KC_0,    KC_GRV,
-    KC_TAB,  KC_Q, KC_W,         KC_F,         KC_P,         KC_B,           KC_J,    KC_L,        KC_U,        KC_Y,         KC_SCLN, KC_DEL,
-    KC_ESC,  KC_A, LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G,           KC_M,    LCTL_T(KC_N), LSFT_T(KC_E),LALT_T(KC_I), KC_O,    KC_QUOT,
-    KC_LSFT, KC_Z, KC_X,         KC_C,         KC_D,         KC_V,           KC_K,    KC_H,        KC_COMM,     KC_DOT,      KC_SLSH, KC_RSFT,
+    _______, KC_1, KC_2,         KC_3,         KC_4,         KC_5,           KC_6,    KC_7,        KC_8,        KC_9,         KC_0,    KC_GRV,
+    _______, KC_Q, KC_W,         KC_F,         KC_P,         KC_B,           KC_J,    KC_L,        KC_U,        KC_Y,         KC_SCLN, _______,
+    _______, KC_A, LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G,           KC_M,    LCTL_T(KC_N), LSFT_T(KC_E),LALT_T(KC_I), KC_O,   KC_QUOT,
+    _______, KC_Z, KC_X,         KC_C,         KC_D,         KC_V,           KC_K,    KC_H,        KC_COMM,     KC_DOT,       KC_SLSH, _______,
                    _______,      _______,      _______,      _______,        _______, _______, _______, _______,
                    _______,      _______,      _______,      _______,        _______, _______, _______, _______
   ),
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3(
-    _______, _______, _______, _______, _______, _______,        _______, _______, _______,_______, _______, _______,
+    KC_PSCR, KC_INS,  KC_SCRL, KC_PAUS, _______, _______,        _______, _______, _______,_______, _______, _______,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
     _______, UNDO,    CUT,     COPY,    PASTE,   FIND,           XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_split_3x6_3(
     _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______, KC_HOME,MLT_LN_UP,MLT_LN_DN, KC_END,_______,        WORD_BACK, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, WORD_FOR,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,
+    KC_HOME,WORD_BACK,MLT_LN_UP,MLT_LN_DN,WORD_FOR,KC_END,       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, XXXXXXX,
     _______, KC_MUTE, KC_VOLU, KC_VOLD, KC_MPLY, _______,        KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
                       _______, _______, _______, _______,        _______,  _______, _______, _______,
                       _______, _______, _______, _______,        _______,  _______, _______, _______
